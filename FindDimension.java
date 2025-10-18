1ec1646a27c4c6c9880726dab8792b234d4549c4
MuhammadSalehJanjua <muhammadsalehjanjua@gmail.com>
2025-10-18 23:03:43 +0500

    Fixed the code optimization

--- FindDimension.java
+++ FindDimension.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 public class FindDimension {
  public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); 
-        System.out.print("Enter the size of the item: "); 
-        double itemSize = scanner.nextDouble(); 
+        double itemSize = 0; 
         double padding = 2.0; 
  
-        double packageDimension = itemSize + 2 * padding; 
+        System.out.print("Enter the size of the item: "); 
+        while (!scanner.hasNextDouble()) { 
+            System.out.println("Invalid input. Please enter a number."); 
+            scanner.next(); // Discard the invalid input 
+            System.out.print("Enter the size of the item: "); 
+        } 
+        itemSize = scanner.nextDouble(); 
  
+        double packageDimension = itemSize + 2 * padding; 
         System.out.println("For an item of size: " + itemSize); 
         System.out.println("The required package dimension is: " + packageDimension); 
         scanner.close(); 
