8fbd42af41170af1ee3cd469dec4f7d4677770d6
MuhammadSalehJanjua <muhammadsalehjanjua@gmail.com>
2025-10-18 22:55:20 +0500

    Changed

--- FindDimension.java
+++ FindDimension.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 public class FindDimension {
  public static void main(String[] args) {
- Scanner sc = new Scanner(System.in);
- System.out.print("Enter item size in cm: ");
- double size = sc.nextDouble();
- double length = size * 2, width = size * 1.5, height = size * 1.2;
- System.out.println("Package Dimensions: " + length + " x " + width + " x " + height);
- sc.close();
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter the size of the item: "); 
+        double itemSize = scanner.nextDouble(); 
+        double padding = 2.0; 
+ 
+        double packageDimension = itemSize + 2 * padding; 
+ 
+        System.out.println("For an item of size: " + itemSize); 
+        System.out.println("The required package dimension is: " + packageDimension); 
+        scanner.close(); 
  }
 }
+ 
